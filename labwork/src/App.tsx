dc1bb505ae2974b9a2e0e89e20b390805768afc0
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-01-12 19:02:48 +0600

    Created ingredients data and type

--- labwork/src/App.tsx
+++ labwork/src/App.tsx
@@ -1,8 +1,27 @@
 import { useState } from 'react';
 import './App.css';
+import { IngredientsData } from './ingredientsData';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className='ingredientsWrap'></div>
+      <div className='burgerWrap'></div>
+    </>
+  );
 }
 
 export default App;
+
+{
+  /* <div className='Burger'>
+  <div className='BreadTop'>
+    <div className='Seeds1'></div>
+    <div className='Seeds2'></div>
+  </div>
+  <div className='Salad'></div>
+  <div className='Cheese'></div>
+  <div className='Meat'></div>
+  <div className='BreadBottom'></div>
+</div>; */
+}
